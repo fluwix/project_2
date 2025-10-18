a5be53afd020dcd98971b9185db88a6fa86fd6ae
kizima <kable660@gmail.com>
2025-10-18 11:37:43 +0300

    second

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,105 +1,106 @@
 fun main() {
     while (true) {
-        println("\n=== МЕНЮ ДЛЯ ЗАДАЧ ===")
-        println("1")
-        println("2")
-        println("3")
-        println("4")
-        println("5")
+        println("\n=== МЕНЮ ===")
+        println("1 - Задача 1")
+        println("2 - Задача 2")
+        println("3 - Задача 3")
         println("0 - Выход")
-        print("Выбери цифру: ")
+        print("Выберите цифру: ")
 
-        when (readLine()!!.toIntOrNull()) {
-            1 -> task1()
-            2 -> task2()
-            3 -> task3()
-            4 -> task4()
-            5 -> task5()
-            0 -> {
-                println("ВЫХОД ИЗ ПРОГРАММЫ")
-                return
-            }
-            else -> println("ВЫБЕРИТЕ ЦИФРУ ОТ 0 ДО 6")
+        val choice = readLine()!!.toInt()
+
+        if (choice == 1) {
+            task1()
+        } else if (choice == 2) {
+            task2()
+        } else if (choice == 3) {
+            task3()
+        } else if (choice == 0) {
+            println("ВЫХОД")
+            break
+        } else {
+            println("Неправильный выбор!")
         }
     }
 }
 
-fun task1(){
-        print("Введите количество строк: ")
-        val rows = readLine()!!.toInt()
-        print("Введите количество столбцов: ")
-        val cols = readLine()!!.toInt()
+fun task1() {
+    print("Сколько строк? ")
+    val rows = readLine()!!.toInt()
+    print("Сколько столбцов? ")
+    val cols = readLine()!!.toInt()
 
-        val matrix = Array(rows) { IntArray(cols) }
-        val uniqueNums = mutableSetOf<Char>()
+    val numbers = Array(rows) { IntArray(cols) }
+    val allDigits = mutableSetOf<Char>()
 
-        println("Введите $rows строк по $cols трехзначных чисел:")
+    println("Вводите числа:")
 
-        for (i in 0 until rows) {
-            for (j in 0 until cols) {
-                print("Элемент [$i][$j]: ")
-                matrix[i][j] = readLine()!!.toInt()
+    for (i in 0 until rows) {
+        for (j in 0 until cols) {
+            print("Число [$i][$j]: ")
+            numbers[i][j] = readLine()!!.toInt()
 
-                val digits = matrix[i][j].toString().toCharArray()
-                uniqueNums.addAll(digits)
+            val numStr = numbers[i][j].toString()
+            for (digit in numStr) {
+                allDigits.add(digit)
             }
-        }
-
-        println("\nВведенный массив:")
-        for (i in 0 until rows) {
-            for (j in 0 until cols) {
-                print("${matrix[i][j]}\t")
-            }
-            println()
-        }
-
-        println("\nВ массиве использовано ${uniqueNums.size} различных цифр")
-        println("Использованные цифры: ${uniqueNums.sorted().joinToString(", ")}")
-    }
-
-fun task2()
-{
-        val matrix = arrayOf(
-            intArrayOf(5, 9, 6, 7, 2),
-            intArrayOf(9, 8, 4, 5, 3),
-            intArrayOf(6, 4, 3, 8, 7),
-            intArrayOf(7, 5, 8, 4, 8),
-            intArrayOf(2, 3, 7, 8, 1)
-        )
-
-        println("Исходный массив 5x5:")
-        printMatrix(matrix)
-
-        val isSymmetric = checkSymmetry(matrix)
-
-        if (isSymmetric) {
-            println("Массив симметричен относительно главной диагонали")
-        } else {
-            println("Массив НЕ симметричен относительно главной диагонали")
         }
     }
 
-    fun printMatrix(matrix: Array<IntArray>) {
-        for (i in matrix.indices) {
-            for (j in matrix[i].indices) {
-                print("${matrix[i][j]}\t")
+    println("\nВаш массив:")
+    for (i in 0 until rows) {
+        for (j in 0 until cols) {
+            print("${numbers[i][j]}\t")
+        }
+        println()
+    }
+
+    println("\nРазных цифр: ${allDigits.size}")
+    println("Цифры: ${allDigits.sorted().joinToString(", ")}")
+}
+
+fun task2() {
+
+    val numbers = arrayOf(
+        intArrayOf(5, 9, 6, 7, 2),
+        intArrayOf(9, 8, 4, 5, 3),
+        intArrayOf(6, 4, 3, 8, 7),
+        intArrayOf(7, 5, 8, 4, 8),
+        intArrayOf(2, 3, 7, 8, 1)
+    )
+
+    println("Массив 5x5:")
+    for (i in 0..4) {
+        for (j in 0..4) {
+            print("${numbers[i][j]}\t")
+        }
+        println()
+    }
+
+    var symmetric = true
+    for (i in 0..4) {
+        for (j in 0..4) {
+            if (numbers[i][j] != numbers[j][i]) {
+                symmetric = false
             }
-            println()
         }
     }
 
-    fun checkSymmetry(matrix: Array<IntArray>): Boolean {
-        val n = matrix.size
-
-        for (i in 0 until n) {
-            for (j in i + 1 until n) {
-                if (matrix[i][j] != matrix[j][i]) {
-                    println("Найдено несоответствие: matrix[$i][$j] = ${matrix[i][j]} != matrix[$j][$i] = ${matrix[j][$i]}")
-                    return false
-                }
-            }
-        }
-
-        return true
+    if (symmetric) {
+        println("Массив симметричный")
+    } else {
+        println("Массив НЕ симметричный")
     }
+}
 
+fun task3() {
+    print("Введите число: ")
+    val number = readLine()!!.toInt()
+
+    val binary = number.toString(2)
+    println("В двоичной системе: $binary")
+}
+
+fun task 4(){
+
+}
