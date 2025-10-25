c3f1ccafd24279b2fea9484e0adc6ef5898b96b9
kizima <kable660@gmail.com>
2025-10-25 11:40:28 +0300

    third commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,6 +4,8 @@ fun main() {
         println("1 - Задача 1")
         println("2 - Задача 2")
         println("3 - Задача 3")
+        println("4 - Задача 4")
+        println("4 - Задача 5")
         println("0 - Выход")
         print("Выберите цифру: ")
 
@@ -15,6 +17,10 @@ fun main() {
             task2()
         } else if (choice == 3) {
             task3()
+        } else if (choice == 4) {
+            task4()
+        } else if (choice == 5) {
+            task5()
         } else if (choice == 0) {
             println("ВЫХОД")
             break
@@ -101,6 +107,180 @@ fun task3() {
     println("В двоичной системе: $binary")
 }
 
-fun task 4(){
+fun task4() {
 
+    println("Введите массив (числа через пробел):")
+    val masOne = readLine()!!
+    println("Введите второй массив (числа через пробел):")
+    val masTwo = readLine()!!
+
+    val parts1 = masOne.split(" ")
+    val parts2 = masTwo.split(" ")
+
+    val arr1 = IntArray(parts1.size)
+    val arr2 = IntArray(parts2.size)
+    
+    var index1 = 0
+    while (index1 < parts1.size) {
+        arr1[index1] = parts1[index1].toInt()
+        index1++
+    }
+
+    var index2 = 0
+    while (index2 < parts2.size) {
+        arr2[index2] = parts2[index2].toInt()
+        index2++
+    }
+
+    val result = natiPeresechenie(arr1, arr2)
+
+    println("Пересечение массивов:")
+    print("[")
+    for (i in result.indices) {
+        print(result[i])
+        if (i < result.size - 1) {
+            print(", ")
+        }
+    }
+    println("]")
+}
+
+fun natiPeresechenie(a: IntArray, b: IntArray): IntArray {
+
+    val copyA = IntArray(a.size)
+    val copyB = IntArray(b.size)
+
+    for (i in a.indices) {
+        copyA[i] = a[i]
+    }
+    for (i in b.indices) {
+        copyB[i] = b[i]
+    }
+
+    val vremennoeHranilishe = IntArray(a.size + b.size)
+    var resultCount = 0
+
+    val usedMarker = -123456789
+
+    for (i in copyA.indices) {
+        for (j in copyB.indices) {
+            if (copyA[i] == copyB[j] && copyB[j] != usedMarker) {
+                vremennoeHranilishe[resultCount] = copyA[i]
+                resultCount++
+                copyB[j] = usedMarker
+                break
+            }
+        }
+    }
+
+    val final = IntArray(resultCount)
+    for (i in 0 until resultCount) {
+        final[i] = vremennoeHranilishe[i]
+    }
+
+    var swapped = true
+    var n = final.size
+    while (swapped) {
+        swapped = false
+        for (i in 1 until n) {
+            if (final[i - 1] > final[i]) {
+                val tmp = final[i - 1]
+                final[i - 1] = final[i]
+                final[i] = tmp
+                swapped = true
+            }
+        }
+        n--
+    }
+
+    return final
+}
+
+fun task5() {
+    println("Введите слова через пробел:")
+
+    val input = readLine()!!
+    val words = input.split(" ")
+
+    val result = groupWords(words.toTypedArray())
+
+    println("Группы слов:")
+    for (group in result) {
+        print("\"")
+        for (i in group.indices) {
+            print(group[i])
+            if (i < group.size - 1) {
+                print("\", \"")
+            }
+        }
+        println("\"")
+    }
+}
+
+fun groupWords(words: Array<String>): List<List<String>> {
+    val groups = mutableListOf<List<String>>()
+    val used = BooleanArray(words.size)
+
+    for (i in words.indices) {
+        if (used[i]) continue
+
+        val currentGroup = mutableListOf<String>()
+        currentGroup.add(words[i])
+        used[i] = true
+
+        for (j in i + 1 until words.size) {
+            if (used[j]) continue
+
+            if (haveSameLetters(words[i], words[j])) {
+                currentGroup.add(words[j])
+                used[j] = true
+            }
+        }
+
+        groups.add(currentGroup)
+    }
+
+    return groups
+}
+
+fun haveSameLetters(word1: String, word2: String): Boolean {
+    if (word1.length != word2.length) return false
+
+    val chars1 = CharArray(word1.length)
+    val chars2 = CharArray(word2.length)
+
+    for (i in word1.indices) {
+        chars1[i] = word1[i]
+    }
+    for (i in word2.indices) {
+        chars2[i] = word2[i]
+    }
+
+    sortChars(chars1)
+    sortChars(chars2)
+
+    for (i in chars1.indices) {
+        if (chars1[i] != chars2[i]) {
+            return false
+        }
+    }
+
+    return true
+}
+
+fun sortChars(chars: CharArray) {
+    var swapped: Boolean
+    var n = chars.size
+    do {
+        swapped = false
+        for (i in 1 until n) {
+            if (chars[i - 1] > chars[i]) {
+                val temp = chars[i - 1]
+                chars[i - 1] = chars[i]
+                chars[i] = temp
+                swapped = true
+            }
+        }
+        n--
+    } while (swapped)
 }
